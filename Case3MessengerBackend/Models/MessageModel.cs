f992c1d03d5f590b19e341ccfce54e31c7d06229
Chittaka <yelena.filatova.2000@list.ru>
2025-09-13 14:32:37 +0300

    add models

---
+++ Case3MessengerBackend/Models/MessageModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Case2
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public int ChatId { get; set; }
+        public int SenderId { get; set; }
+        public string Text { get; set; }
+        public DateTime SentAt { get; set; }
+        public bool IsRead { get; set; }
+        public Chat Chat { get; set; } = null!;
+        public User Sender { get; set; } = null!;
+    }
+
+    public record MessageDto(int Id, int ChatId, int SenderId, string SenderAvatarUrl, string Text, DateTime SentAt, bool IsRead,string Sender);
+
+    public record SendMessageRequest(int ChatId, int SenderId, string Text);
+}
---
+++ Case3MessengerBackend/Models/UserChatModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Case2
+{
+    public class UserChat
+    {
+            public int UserId { get; set; }
+            public User User { get; set; }
+
+            public int ChatId { get; set; }
+            public Chat Chat { get; set; }
+    }
+}
